d52ee1197b92e726a1d3e1a49f9a0be30793341b
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-16 14:44:08 +0100

    Creato il file php e il file drawio
    Creato il file php e il file drawio

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// Crea un diagramma per una tabella di db che rappresenti gli Users di un blog.
+// Crea una classe User che rappresenti quella tabella, e usala per stampare in pagina i dati di alcuni Users.
+// Il database e la tabella non devono essere realmente creati.
+
+
+
+?>
